5f3c6530707bbc81935554cc91a4d8eb5102b609
Paweł F <pawel605@gmail.com>
2022-11-23 02:51:05 +0100

    Fix error with progressBar

--- src/components/Player/components/ProgressBar/ProgressBar.component.tsx
+++ src/components/Player/components/ProgressBar/ProgressBar.component.tsx
@@ -3,15 +3,15 @@ import React from "react";
 import styles from "./ProgressBar.module.css";
 
 interface Props {
-  progress: () => void;
-}
+  progress: number;
+  }
 
 const ProgressBar = ({ progress }: Props) => {
   return (
     <div className={styles.containerWithProgressBar}>
       <div
         className={styles.progressBar}
-        style={{ width: progress * 100 + '%' }}
+        style={{ width: `${progress * 100}%` }}
       ></div>
     </div>
   );
